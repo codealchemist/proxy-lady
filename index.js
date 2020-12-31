4103eafa50b1e9dc250da935234e006293435d87
Alberto Miranda <amiranda@firstarriving.com>
2020-12-30 22:09:08 -0300

    WIP

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-const SimplePeer = require('simple-peer')
 const Proxy = require('./proxy')
 const WebSocketClient = require('./websocket')
+const Peer = require('./webrtc')
 
 const port = process.env.PORT || 8080
 const proxy = new Proxy()
-proxy.create().listen(port)
+const peer = new Peer()
+proxy.listen(port, '192.168.1.17')
 
+//------------------------------------------------
+// Connect to remote device.
 const ws = new WebSocketClient()
 // ws.connect('wss://proxy-lady.openode.io:443')
 ws.connect('wss://proxy-lady.herokuapp.com:443')
+
+ws.onMessage(message => {
+	peer.signal(message)
+})
+
+peer.onSignal(signal => {
+	ws.send(signal)
+}) 
+//------------------------------------------------
+
+proxy.onRequest((req, res) => {
+	const { url, headers, body, method, httpVersion } = req
+	peer.send({ url, headers, method, httpVersion })
+})
+
+proxy.onConnect((req, socket, head) => {
+	const { url, headers, method, httpVersion } = req
+	peer.send({ url, headers, method, httpVersion })
+})
--- websocket.js
+++ websocket.js
@@ -1,43 +1,84 @@
 const WebSocket = require('ws')
 
 class WebSocketClient {
-  constructor(options = {}) {
+  constructor() {
     this.debug = true
-    this.options = options
+    this.isConnected = false
+    this.retries = 0
+    this.maxRetries = 10
+    this.queue = []
   }
 
   connect(serverUrl) {
-    this.log(`Connecting to ${serverUrl}...`)
-    this.client = new WebSocket(serverUrl, this.options)
-    this.setEvents()
+    this.serverUrl = serverUrl
+    this.ws = new WebSocket(serverUrl)
+    this.ws.onopen = () => {
+      this.log('WS OPEN')
+      this.isConnected = true
+
+      if (this.queue.length) {
+        this.log('Send queued messages...')
+        this.sendQueued()
+      }
+    }
+  
+    this.ws.onmessage = (e) => {
+      const message = JSON.parse(e.data)
+      this.log('WS MESSAGE:', message)
+      if (message.type === 'disconnect') return
+      if (typeof this.onMessageCallback === 'function') {
+        this.onMessageCallback(message)
+      }
+    }
+  
+    this.ws.onclose = (e) => {
+      this.isConnected = false
+      this.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason)
+      setTimeout(() => {
+        this.connect(this.serverUrl)
+      }, 1000)
+    }
+  
+    this.ws.onerror = (err) => {
+      this.log('ERROR: Closing socket', err.message)
+      this.ws.close()
+    }
+  
+    return this
   }
 
-  setEvents() {
-    this.client.on('connect', () => {
-      this.log('Connected!')
-    })
-
-    this.client.on('open', () => {
-      this.log('Open')
-    })
-
-    this.client.on('message', (data) => {
-      this.log('Message', data)
-    })
-
-    this.client.on('close', () => {
-      this.log('CLOSED')
-      this.connect()
-    });
+  sendQueued() {
+    if (!this.isConnected) return
+    const msgCount = this.queue.length
+    if (!msgCount) {
+      this.log('sendQueued: Queue is empty.')
+      return
+    }
+    this.log(`sendQueued: ${msgCount} messages left`)
+    const message = this.queue.shift()
+    this.send(message)
+    setTimeout(() => this.sendQueued(), 1000)
   }
 
   send(message) {
-    this.client.send(JSON.stringify(message))
+    // Enqueue message. Send it when connected.
+    if (!this.isConnected) {
+      this.queue.push(message)
+      this.log(`${this.queue.length} messages enqueued`)
+      return
+    }
+
+    this.ws.send(JSON.stringify(message))
+    return this
+  }
+
+  onMessage(callback) {
+    this.onMessageCallback = callback
   }
 
   log() {
     if (!this.debug) return
-    console.log('[ WS-Client ]', ...arguments)
+    console.log('[ WS ]', ...arguments)
   }
 }
 
